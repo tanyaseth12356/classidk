7de452483b5b3ba11363facef58ac3e4160ab055
tanyaseth12356 <140946568+tanyaseth12356@users.noreply.github.com>
2024-04-02 17:32:09 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  clown_nose = loadImage ('https://i.postimg.cc/BZc8HpXP/clown-face-removebg-preview.png')
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video= createCapture(VIDEO)
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+  }
+}
+
+function modelLoaded()
+{
+  console.log('PoseNet is Initialized');
+}
+
+function draw() 
+{
+ image(video, 0, 0, 300, 300);
+ fill(255,0,0);
+ stroke(255,0,0);
+ circle(noseX, noseY, 20);
+ image(clown_nose, noseX-15, noseY-35, 250, 150);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
